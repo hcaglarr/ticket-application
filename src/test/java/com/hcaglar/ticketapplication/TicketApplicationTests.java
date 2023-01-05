97a3945a1874bcd4ad821e8f372d20c5c983b413
hcaglar <huseyin-caglar@outlook.com>
2023-01-05 22:57:24 +0300

    project starting point

---
+++ src/test/java/com/hcaglar/ticketapplication/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcaglar.ticketapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
